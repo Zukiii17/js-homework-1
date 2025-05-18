491683ed8628e6c4a430d3336e8d2fc50f2800d6
Guram Shavgulidze <guramshavgulidze@gmail.com>
2025-05-18 15:14:24 +0400

    js-homework-1

---
+++ script.js
@@ -0,0 +1,27 @@
+// Guram Shavgulidze 2007/01/01
+
+const firstName = "Guram";
+const lastName = "Shavgulidze";
+const email = "guramshavgulidze@gmail.com";
+const phoneNumber = "599-77-62-62";
+let age = 18;
+let hobby = "Boxing";
+const isStudent = true;
+
+console.log("First Name: " + firstName);
+console.log("Last Name: " + lastName);
+console.log("Email: " + email);
+console.log("Phone Number: " + phoneNumber);
+console.log("Age: " + age);
+console.log("Hobby: " + hobby);
+console.log("Is Student: " + isStudent);
+
+hobby = "Programming";
+age = age + 1;
+
+console.log("Updated Hobby: " + hobby);
+console.log("Updated Age: " + age);
+
+// Wrote personal information variables.
+// Used console.log to display readable messages with variable values.
+// Updated the hobby and age variables to display user data changes.
